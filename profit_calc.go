0f400baa330e90ab16e7f777e0241ec4bc03d4e1
Albert Pak <al@albert-pak.com>
2024-03-16 08:08:28 -0400

    feat: add erro handling to inputs and save results to a file

--- profit_calc.go
+++ profit_calc.go
@@ -1,17 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenueAmount := getUserInput("Revenue: ")
-	expensesAmount := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	revenueAmount, revenueAmountErr := getUserInput("Revenue: ")
+	expensesAmount, expensesAmountErr := getUserInput("Expenses: ")
+	taxRate, taxRateError := getUserInput("Tax Rate: ")
+
+	if revenueAmountErr != nil || expensesAmountErr != nil || taxRateError != nil {
+		fmt.Println("Must be positive number")
+		return
+	}
 
 	earningsBeforeTaxes, profit, ratio := calculateFinancials(revenueAmount, expensesAmount, taxRate)
 
 	fmt.Println("EBT is ", earningsBeforeTaxes)
 	fmt.Println("Your profit is ", profit)
 	fmt.Println("Your ratio is ", ratio)
+
+	storeResults(earningsBeforeTaxes, profit, ratio)
+}
+
+func storeResults(earningsBeforeTaxes, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.2f\nProfit: %.2f\nRatio: %.3f\n", earningsBeforeTaxes, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
 }
 
 func calculateFinancials(revenueAmount, expensesAmount, taxRate float64) (float64, float64, float64) {
@@ -21,9 +37,14 @@ func calculateFinancials(revenueAmount, expensesAmount, taxRate float64) (float6
 	return ebt, profit, ratio
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-	return userInput
+
+	if userInput <= 0 {
+		return 0, errors.New("value must be positive")
+	}
+
+	return userInput, nil
 }
